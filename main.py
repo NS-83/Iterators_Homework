b84e89ac6d1876f7967bfb32f18a1dd1f6eb6664
Николай Степанов <n.stepanov83@gmail.com>
2026-08-02 23:15:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import types
+
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        flat_list = []
+        self.list_of_list = create_flat_list(list_of_list, flat_list)
+        self.list_len = len(self.list_of_list)
+        self.cursor = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.cursor += 1
+        if self.cursor == self.list_len:
+            raise StopIteration
+        return self.list_of_list[self.cursor]
+
+def create_flat_list(source_list, result_list):
+    for item in source_list:
+        if isinstance(item, list):
+            create_flat_list(item, result_list)
+        else:
+            result_list.append(item)
+    return result_list
+
+def flat_generator(list_of_lists):
+    flat_list = []
+    result = create_flat_list(list_of_lists, flat_list)
+    result_len = len(result)
+    for cursor in range(result_len):
+        yield result[cursor]
+
+
+def test_3():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_3()
+    test_4()
